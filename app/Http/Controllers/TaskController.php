acad29bab4773557d72514dc5d1aa74072a80b72
Elias Gasparis <iliasgas@gmail.com>
2020-04-18 12:36:56 +0300

    added font-awesome library

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,11 +17,7 @@ class TaskController extends Controller
         $this->tasks = $tasks;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //Display a listing of the resource.
     public function index(Request $request)
     {
         $user_tasks = $this->tasks->forUser($request->user());
@@ -31,22 +27,13 @@ class TaskController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //Show the form for creating a new resource.
     public function create()
     {
         return view('tasks.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    //Store a newly created resource in storage.
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -60,47 +47,26 @@ class TaskController extends Controller
         return redirect('/tasks');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //Display the specified resource.
     public function show(Task $task)
     {
         //do nothing (we don't need this route)
         return redirect('/tasks');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //Show the form for editing the specified resource.
     public function edit(Task $task)
     {
         //TODO
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //Update the specified resource in storage.
     public function update(Request $request, Task $task)
     {
         //TODO
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //Remove the specified resource from storage.
     public function destroy(Task $task)
     {
         $this->authorize('destroy', $task);
--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -11,26 +11,14 @@ class TaskPolicy
     use HandlesAuthorization;
 
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Task  $task
-     * @return mixed
-     */
+    //Determine whether the user can update the model.
     public function update(User $user, Task $task)
     {
         //TODO
         return false;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Task  $task
-     * @return mixed
-     */
+    //Determine whether the user can delete the model.
     public function destroy(User $user, Task $task)
     {
         return $user->id === $task->user_id;
