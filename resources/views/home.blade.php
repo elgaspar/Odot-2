3238b33e2c7de126bf923ecf5d3d150e14e2f52f
Elias Gasparis <iliasgas@gmail.com>
2020-04-19 12:27:11 +0300

    added view-tasks button in home page of logged in user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (session('status'))
-        <div class="alert alert-success" role="alert">
-            {{ session('status') }}
-        </div>
-    @endif
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+    {{ session('status') }}
+</div>
+@endif
 
-    You are logged in!
+You are logged in!
+
+<br>
+<br>
+<a href="{{ route('tasks.index') }}" class="btn btn-primary">View Tasks</a>
+
 @endsection
