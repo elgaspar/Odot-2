fe66371a5ea36814b156b4601510fb0dfd852883
Elias Gasparis <iliasgas@gmail.com>
2020-04-22 10:03:37 +0300

    fixed home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-@if (session('status'))
-<div class="alert alert-success" role="alert">
-    {{ session('status') }}
-</div>
-@endif
 
 You are logged in!
 
 <br>
 <br>
-<a href="{{ route('tasks.index') }}" class="btn btn-primary">View Tasks</a>
+<a href="{{ route('projects.index') }}" class="btn btn-primary">View Projects</a>
 
 @endsection
