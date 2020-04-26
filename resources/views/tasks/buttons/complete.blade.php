7f031b1dd15203a619b10743f1078a204885b4b0
Elias Gasparis <iliasgas@gmail.com>
2020-04-26 09:48:02 +0300

    Fixed a bug where "mark completed/incomplete" were not working

--- resources/views/tasks/buttons/complete.blade.php
+++ resources/views/tasks/buttons/complete.blade.php
@@ -2,6 +2,7 @@
     @csrf
     @method('PUT')
     <input type="hidden" name="id" value="{{ $task->id }}">
+    <input type="hidden" name="name" value="{{ $task->name }}">
     <input type="hidden" name="is_completed" value="{{ $task->is_completed ? 0 : 1 }}">
     <button type="submit" class="btn task-complete-button action-btn">
         <i class="fas {{ $task->is_completed ? 'fa-check-circle' : 'fa-circle' }}"></i>
